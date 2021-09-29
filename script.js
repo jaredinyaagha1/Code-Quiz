f1e5f09f41eb3fe270f75b8a64094a6f17303f3d
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-09-29 10:20:26 -0400

    First JS and CSS commits

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let startbtn = document.getElementById("start-button");
+let nextbtn = document.getElementById("next-button");
+startbtn.addEventListener("click", function () {
+    startbtn.classList.add("hide")
+    nextbtn.classList.remove("hide")
+})
+
+const questions = [
+    {
+        question:"soadifdspo"
+        answers:
+        answer:},
+    {
+        question:"soadifdspo"
+        answers:
+        answer:}
+    ]
